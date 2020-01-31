c8194468d457640fcc9317800adaf6e618f913c7
zahid hussain <zahidbarahvi821@gmail.com>
2020-01-31 20:15:52 +0500

    added bulk booking page and maked functional

--- src/components/dashboard/venueBooking/index.js
+++ src/components/dashboard/venueBooking/index.js
@@ -12,6 +12,7 @@ import "../administration/adminDashboard.css"
 import AllBooking from "./viewAllBooking";
 import AdminBooking from "./viewAdminBooking";
 import PriorityBooking from "./viewPriorityBooking/PriorityBooking";
+import BulkBooking from "./bulkBooking";
 // import Users from "./users";
 // import Venues from "./venues";
 export default function VenueDashboard() {
@@ -31,7 +32,9 @@ export default function VenueDashboard() {
                         history.goBack();
                     }} > <svg className="btn-back-svg" width="10" height="14"  viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.07874 6.46949L7.15218 0.396367C7.44499 0.103555 7.91999 0.103555 8.2128 0.396367L8.92124 1.1048C9.21374 1.3973 9.21405 1.87105 8.92249 2.16418L4.10905 6.9998L8.92218 11.8357C9.21405 12.1289 9.21343 12.6026 8.92093 12.8951L8.21249 13.6036C7.91968 13.8964 7.44468 13.8964 7.15187 13.6036L1.07874 7.53012C0.785928 7.2373 0.785928 6.7623 1.07874 6.46949Z" fill="white"/>
-                    </svg>Back</button>
+                    </svg>
+                    Back
+               </button>
                
               <hr></hr>
               </div>
@@ -46,7 +49,7 @@ export default function VenueDashboard() {
                         <Route path="/venuebooking/booking" render={() => <AllBooking/>} />
                         <Route path="/venuebooking/admin" render={() => <AdminBooking/>} />
                         <Route path="/venuebooking/priority" render={() => <PriorityBooking/>} />
-                        <Route path="/venuebooking/bulk" render={() => <div>testing5</div>} />
+                        <Route path="/venuebooking/bulk" render={() => <BulkBooking/>} />
                       </Switch>
                     </DashBardContent>
               </Col>
--- src/index.js
+++ src/index.js
@@ -4,70 +4,24 @@ import './index.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./components/ProtectedRoutes/privateRoute";
 import 'antd/dist/antd.css';
-//import "./assets/css/material-dashboard-react.css?v=1.8.0";
+
 import "bootstrap";
 import "bootstrap/js/dist/util";
 import "bootstrap/js/dist/dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CreateUser from "./components/dashboard/administration/users/CreateUser";
 
-// import  Store  from "./Redux/store";
-// import { Provider } from "react-redux";
-//import setAuthorizationToken from "./Redux/utils/setAuthToken";
 import axios from "axios";
 import login from "./views/Authentication/login.js"
 import Dashboard from "./components/dashboard";
 import AdminDashboard from "./components/dashboard/administration";
 import VenueDashboard from "./components/dashboard/venueBooking";
-//import { ProtectedRoute } from "./routers/ProtectedRoute";
 
 // axios.defaults.baseURL = "http://localhost:4200/api";
  axios.defaults.baseURL = "https://venue-booking-api.herokuapp.com/api";
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
-// import { createBrowserHistory } from "history";
-// import { Router, Route, Switch, Redirect } from "react-router-dom";
-// import setAuthToken from "./Redux/utils/setAuthToken";
-
-// // core components
-// import Admin from "./layouts/Admin.js";
-// import login from "./views/Authentication/login.js"
-// import Dashboard from "./components/dashboard";
-
-
-//const hist = createBrowserHistory();
-
-// Check for token
-// if (localStorage.jwtToken) {
-//     // Set auth token header auth
-//     setAuthToken(localStorage.jwtToken);
-//     // Decode token and get user info and exp
-//     const decoded = jwt_decode(localStorage.jwtToken);
-//     // Set user and isAuthenticated
-//     store.dispatch(setCurrentUser(decoded));
-//     // Check for expired token
-//     const currentTime = Date.now() / 1000;
-//     if (decoded.exp < currentTime) {
-//         // Logout user
-//         store.dispatch(logoutUser());
-//         // Clear current Profile
-//         store.dispatch(clearCurrentProfile());
-//         // Redirect to login
-//         window.location.href = '/login';
-//     }
-// }
 
 ReactDOM.render(
-    // <Router history={hist}>
-    //     <Switch>
-    //         {/* <Route path="/admin" component={ Admin } /> */}
-    //         <Route exact path="/dashboard1" component={Dashboard} />
-    //          <Route exact path="/dashboard1/admin" component={AdminDashboard} />
-    //         {/* <Route path="/login" component={ login } />
-    //         <Redirect from="/" to="/login" /> */}
-    //     </Switch>
-    // </Router>,
     <div className="App">
-      {/*<Provider store={Store}>*/}
         <Router>
           <Switch>
             <Route
@@ -82,17 +36,6 @@ ReactDOM.render(
             <PrivateRoute path="/venuebooking" component={VenueDashboard} />
           </Switch>
         </Router>
-      {/*</Provider>*/}
     </div>,
     document.getElementById("root")
 );
-
-
-// axios.defaults.baseURL = process.env.SERVER_URL;
-//setAuthorizationToken(localStorage.jwtToken);
-// function App() {
-//   return (
-//   );
-// }
-
-// export default App;
